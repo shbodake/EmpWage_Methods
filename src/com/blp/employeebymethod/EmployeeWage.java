090b4b66462e41bfdefe304918ad75ac2e5c75f8
shbodake <sayalibodake28@gmail.com>
2021-10-28 01:05:42 +0530

    Added: Employee Present or Absent

--- src/com/blp/employeebymethod/EmployeeWage.java
+++ src/com/blp/employeebymethod/EmployeeWage.java
@@ -1,4 +1,13 @@
 package com.blp.employeebymethod;
 
 public class EmployeeWage {
+    public static void main(String[] args) {
+        int IS_FULL_TIME = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_FULL_TIME) {
+            System.out.println("employee is Present");
+        } else {
+            System.out.println("employee is Absent");
+        }
+    }
 }
